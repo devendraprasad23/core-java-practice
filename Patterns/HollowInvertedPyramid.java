187bba356e49b925249dc31a3c0534e708769a3c
Devendra Prasad Chinnegowlla <147643160+devendraprasad23@users.noreply.github.com>
2026-01-22 15:29:49 +0530

    Create HollowInvertedPyramid.java

---
+++ Patterns/HollowInvertedPyramid.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class HollowInvertedPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int s = 1; s <= n - i; s++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+
+                if (j == 1 || j == (2 * i - 1) || i == n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
