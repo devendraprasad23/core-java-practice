0277dabd245944c1842fcc0c70bba1e4e74e35c1
Devendra Prasad Chinnegowlla <147643160+devendraprasad23@users.noreply.github.com>
2026-01-22 15:20:13 +0530

    Add DuplicateCharacters class to find duplicates

---
+++ Strings/DuplicateCharacters.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class DuplicateCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String str = sc.next();
+
+        char[] ch = str.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            int count = 1;
+
+            for (int j = i + 1; j < ch.length; j++) {
+                if (ch[i] == ch[j] && ch[i] != '0') {
+                    count++;
+                    ch[j] = '0';
+                }
+            }
+
+            if (count > 1 && ch[i] != '0')
+                System.out.println(ch[i] + " = " + count);
+        }
+    }
+}
